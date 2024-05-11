e7fb4dc9be70f66d1ed4ea160930273afe914ee6
Leonardo Brandão <contato.leonardobbf@gmail.com>
2024-05-11 09:20:26 -0300

    Adicionando laço de repetição e tratamento de exceções

--- main.py
+++ main.py
@@ -20,34 +20,48 @@ GOOGLE_API_KEY = input("Insira a GOOGLE API KEY: ")
 # classificador de imagens em busca de incêndio
 chat = chat_config(GOOGLE_API_KEY)
 
-# Coleta a opção do usuário e define o URL até a câmera de onde a imagem será retirada
-print("Escolha qual camera deseja avaliar:")
-print("\t 1 - Brasilia - Esplanada - https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-esplanada")
-print("\t 2 - Brasilia - Estádio - https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-mane-garrincha")
-print("\t 3 - Brasilia - Parque - https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-parque-da-cidade")
-escolha = input()
+in_loop = True
+while in_loop is True:
+    try:
+        # Coleta a opção do usuário e define o URL até a câmera de onde a imagem será retirada
+        print("Escolha qual camera deseja avaliar:")
+        print("\t 1 - Brasilia - Esplanada - https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-esplanada")
+        print("\t 2 - Brasilia - Estádio - https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-mane-garrincha")
+        print("\t 3 - Brasilia - Parque - https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-parque-da-cidade")
+        print("\t 4 - Outra câmera")
+        print("\t 5 - Sair\n")
+        escolha = input()
 
-if escolha == "1":
-    source_path = "https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-esplanada"
-elif escolha == "2":
-    source_path = "https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-mane-garrincha"
-elif escolha == "3":
-    source_path = "https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-parque-da-cidade"
+        if escolha == "1":
+            source_path = "https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-esplanada"
+        elif escolha == "2":
+            source_path = "https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-mane-garrincha"
+        elif escolha == "3":
+            source_path = "https://www.climaaovivo.com.br/df/brasilia/combo-livre-brasil-21-parque-da-cidade"
+        elif escolha == "4":
+            source_path = input('Insira o link até a camera do ClimaAoVivo: ')
+        elif escolha == "5":
+            in_loop = False
+            break
 
-# Captura uma imagem do stream da camera escolhida
-# salvando-a como um arquivo .jpg no disco local
-img_file, img_frame = capture_handler(source_path=source_path)
+        # Captura uma imagem do stream da camera escolhida
+        # salvando-a como um arquivo .jpg no disco local
+        img_file, img_frame = capture_handler(source_path=source_path)
 
-# Envia a imagem pra análise
-chat.send_message(genai.upload_file("./frame.jpg"))
+        # Envia a imagem pra análise
+        chat.send_message(genai.upload_file("./frame.jpg"))
 
-# Define o resultado em texto, inserindo a data atual
-model_ret = loads(chat.last.text)
-dt = datetime.today()
-text = f"{dt} - {str(model_ret['result']).upper()}"
+        # Define o resultado em texto, inserindo a data atual
+        model_ret = loads(chat.last.text)
+        dt = datetime.today()
+        text = f"{dt} - {str(model_ret['result']).upper()}"
 
-# Apresenta a imagem na tela junto com o resultado em texto
-print_img_text(frame=img_frame, text=text)
+        # Apresenta a imagem na tela junto com o resultado em texto
+        print_img_text(frame=img_frame, text=text)
+        
+    except Exception as ex:
+        print(f"[ERROR] {ex}")
+        break
 
 
 
